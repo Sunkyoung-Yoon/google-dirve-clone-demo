02c42b019ab31dcee44afa8bf55f83e4aa6b33a2
sunkyoung <yskkjdh@gmail.com>
2025-02-12 18:40:42 +0900

    feat : Data component에 firebase에 업로드된 파일 목록 조회 및 파일 오픈  기능 구현

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -5,6 +5,9 @@ import ListIcon from "@mui/icons-material/List";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, onSnapshot } from "firebase/firestore";
 
 const DataContainer = styled.div`
   flex: 1 1;
@@ -75,6 +78,38 @@ const DataListRow = styled.div`
 `;
 
 const Data = () => {
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "myfiles"), // "myfiles" 컬렉션을 참조
+      (snapshot) => {
+        setFiles(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      },
+      (error) => {
+        console.error("Error getting documents: ", error);
+      }
+    );
+
+    // 컴포넌트가 언마운트될 때 리스너 제거
+    return () => unsubscribe();
+  }, []);
+
+  // bytes 변환 (1024 : 1KB, 1048576 : 1MB)
+  const changeBytes = (bytes, decimals = 2) => {
+    if (bytes === 0) return "0 Bytes";
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  };
+
   return (
     <DataContainer>
       <DataHeader>
@@ -89,14 +124,12 @@ const Data = () => {
       </DataHeader>
       <div>
         <DataGrid>
-          <DataFile>
-            <InsertDriveFileIcon />
-            <p>File Name</p>
-          </DataFile>
-          <DataFile>
-            <InsertDriveFileIcon />
-            <p>File Name</p>
-          </DataFile>
+          {files.map((file) => (
+            <DataFile key={file.id}>
+              <InsertDriveFileIcon />
+              <p>{file.data.filename}</p>
+            </DataFile>
+          ))}
         </DataGrid>
         <div>
           <DataListRow>
@@ -115,14 +148,20 @@ const Data = () => {
               <b>File Size</b>
             </p>
           </DataListRow>
-          <DataListRow>
-            <p>
-              Name <InsertDriveFileIcon />
-            </p>
-            <p>Me </p>
-            <p>Yesterday</p>
-            <p>1 GB</p>
-          </DataListRow>
+          {files.map((file) => (
+            <DataListRow key={file.id}>
+              <a href={file.data.fileURL} target="_blank">
+                <p>
+                  <InsertDriveFileIcon /> {file.data.filename}
+                </p>
+              </a>
+              <p>Owner</p>
+              <p>
+                {new Date(file.data.timestamp?.seconds * 1000).toUTCString()}
+              </p>
+              <p>{changeBytes(file.data.size)}</p>
+            </DataListRow>
+          ))}
         </div>
       </div>
     </DataContainer>
