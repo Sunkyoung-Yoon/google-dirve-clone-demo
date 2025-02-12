60fa105061cc0506395c35f488337477fa5c7128
sunkyoung <yskkjdh@gmail.com>
2025-02-12 15:58:39 +0900

    design : 파일 업로드 모달창 디자인

--- src/components/Siderbar.jsx
+++ src/components/Siderbar.jsx
@@ -8,6 +8,9 @@ import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import CloudQueueIcon from "@mui/icons-material/CloudQueue";
 
+import { Modal } from "@mui/material";
+import { useState } from "react";
+
 const SidebarContainer = styled.div`
   margin-top: 10px;
 `;
@@ -61,53 +64,111 @@ const SidebarOption = styled.div`
   }
 `;
 
+const ModalPopup = styled.div`
+  top: 50%;
+  background-color: #fff;
+  width: 500px;
+  margin: 0px auto;
+  position: relative;
+  transform: translateY(-50%);
+  padding: 10px;
+  border-radius: 10px;
+`;
+
+const ModalHeading = styled.div`
+  text-align: center;
+  border-bottom: 1px solid lightgray;
+  height: 40px;
+`;
+
+const ModalBody = styled.div`
+  input.modal__submit {
+    width: 100%;
+    background: darkmagenta;
+    padding: 10px 20px;
+    color: #fff;
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    font-size: 16px;
+    border: 0;
+    outline: 0;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-top: 20px;
+  }
+  input.modal__file {
+    background: whitesmoke;
+    padding: 20px;
+    color: #000;
+    display: block;
+    margin-top: 20px;
+  }
+`;
+
 const Siderbar = () => {
+  const [open, setOpen] = useState(true);
+
   return (
-    <SidebarContainer>
-      <SidebarBtn>
-        <button>
-          <img src="data:image/svg+xml;charset=UTF-8,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 width=%2236%22 height=%2236%22 viewBox=%220 0 36 36%22%3E%3Cpath fill=%22%2334A853%22 d=%22M16 16v14h4V20z%22/%3E%3Cpath fill=%22%234285F4%22 d=%22M30 16H20l-4 4h14z%22/%3E%3Cpath fill=%22%23FBBC05%22 d=%22M6 16v4h10l4-4z%22/%3E%3Cpath fill=%22%23EA4335%22 d=%22M20 16V6h-4v14z%22/%3E%3Cpath fill=%22none%22 d=%22M0 0h36v36H0z%22/%3E%3C/svg%3E" />
-          <span>New</span>
-        </button>
-      </SidebarBtn>
-      <SidebarOptions>
-        <SidebarOption>
-          <MobileScreenShareIcon />
-          <span>My Drive</span>
-        </SidebarOption>
-        <SidebarOption>
-          <DevicesIcon />
-          <span>Computers</span>
-        </SidebarOption>
-        <SidebarOption>
-          <PeopleAltOutlinedIcon />
-          <span>Shared with me</span>
-        </SidebarOption>
-        <SidebarOption>
-          <QueryBuilderOutlinedIcon />
-          <span>Recent</span>
-        </SidebarOption>
-        <SidebarOption>
-          <StarBorderOutlinedIcon />
-          <span>Starred</span>
-        </SidebarOption>
-        <SidebarOption>
-          <DeleteOutlineOutlinedIcon />
-          <span>Trash</span>
-        </SidebarOption>
-      </SidebarOptions>
-      <hr />
-      <SidebarOptions>
-        <div class="option">
-          <CloudQueueIcon />
-          <span>Storage</span>
-        </div>
-        <div className="progress_bar">
-          <progress size="tiny" value="50" max="100" />
-          <span>105GB of 200GB used</span>
-        </div>
-      </SidebarOptions>
-    </SidebarContainer>
+    <>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <ModalPopup>
+          <form>
+            <ModalHeading>
+              <h3>Select file you want to upload</h3>
+            </ModalHeading>
+            <ModalBody>
+              <input type="file" className="modal__file" />
+              <input type="submit" className="modal__submit" />
+            </ModalBody>
+          </form>
+        </ModalPopup>
+      </Modal>
+      <SidebarContainer>
+        <SidebarBtn>
+          <button>
+            <img src="data:image/svg+xml;charset=UTF-8,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 width=%2236%22 height=%2236%22 viewBox=%220 0 36 36%22%3E%3Cpath fill=%22%2334A853%22 d=%22M16 16v14h4V20z%22/%3E%3Cpath fill=%22%234285F4%22 d=%22M30 16H20l-4 4h14z%22/%3E%3Cpath fill=%22%23FBBC05%22 d=%22M6 16v4h10l4-4z%22/%3E%3Cpath fill=%22%23EA4335%22 d=%22M20 16V6h-4v14z%22/%3E%3Cpath fill=%22none%22 d=%22M0 0h36v36H0z%22/%3E%3C/svg%3E" />
+            <span>New</span>
+          </button>
+        </SidebarBtn>
+        <SidebarOptions>
+          <SidebarOption>
+            <MobileScreenShareIcon />
+            <span>My Drive</span>
+          </SidebarOption>
+          <SidebarOption>
+            <DevicesIcon />
+            <span>Computers</span>
+          </SidebarOption>
+          <SidebarOption>
+            <PeopleAltOutlinedIcon />
+            <span>Shared with me</span>
+          </SidebarOption>
+          <SidebarOption>
+            <QueryBuilderOutlinedIcon />
+            <span>Recent</span>
+          </SidebarOption>
+          <SidebarOption>
+            <StarBorderOutlinedIcon />
+            <span>Starred</span>
+          </SidebarOption>
+          <SidebarOption>
+            <DeleteOutlineOutlinedIcon />
+            <span>Trash</span>
+          </SidebarOption>
+        </SidebarOptions>
+        <hr />
+        <SidebarOptions>
+          <div class="option">
+            <CloudQueueIcon />
+            <span>Storage</span>
+          </div>
+          <div className="progress_bar">
+            <progress size="tiny" value="50" max="100" />
+            <span>105GB of 200GB used</span>
+          </div>
+        </SidebarOptions>
+      </SidebarContainer>
+    </>
   );
 };
 
